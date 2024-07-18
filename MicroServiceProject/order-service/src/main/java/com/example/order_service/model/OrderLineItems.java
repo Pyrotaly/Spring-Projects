24ee34c6759b2b2712976026430a9a902595dd6b
Minh13897 <mvn5442@psu.edu>
2024-07-18 12:20:20 -0400

    Starting Order Service

---
+++ MicroServiceProject/order-service/src/main/java/com/example/order_service/model/OrderLineItems.java
@@ -0,0 +1,28 @@
+package com.example.order_service.model;
+
+import jakarta.annotation.Generated;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@Entity
+@Table(name = ("t_order_line_items"))
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class OrderLineItems {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String orderNumber;
+    private List<OrderLineItems> orderLineItemsList;
+}
