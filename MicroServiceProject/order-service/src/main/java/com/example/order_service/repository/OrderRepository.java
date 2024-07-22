7435724a059cf3be95e3af5c18bd27c694bbd035
Minh13897 <mvn5442@psu.edu>
2024-07-22 15:10:25 -0400

    Restart on Order Microservice
    Following updated guide

--- MicroServiceProject/order-service/src/main/java/com/example/order_service/repository/OrderRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.order_service.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.order_service.model.Order;
-
-public interface OrderRepository extends JpaRepository<Order, Long> {
-
-}
